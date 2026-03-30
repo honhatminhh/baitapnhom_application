7b49b8befd1b63c5c3dd49c61b6d6a38b52da0ce
ASUS <ASUS@LAPTOP-IU3EN8M7>
2026-03-30 21:35:39 +0700

    commit 1

--- src/bt_nhom_group5/BT_Nhom_Group5.java
+++ src/bt_nhom_group5/BT_Nhom_Group5.java
@@ -1,20 +1,92 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package bt_nhom_group5;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.*;
 
-/**
- *
- * @author Ho Nhat Minh
- */
-public class BT_Nhom_Group5 {
+public abstract class BT_Nhom_Group5 {
 
-    /**
-     * @param args the command line arguments
-     */
+    Scanner s = new Scanner(System.in);
+
+    private String hoVaTen;
+    private Date ngaySinh;
+    private String gioiTinh;
+    private int cmnd;
+
+    public BT_Nhom_Group5() {
+    }
+
+    public BT_Nhom_Group5(String hoVaTen, Date ngaySinh, String gioiTinh, int cmnd) {
+        this.hoVaTen = hoVaTen;
+        this.ngaySinh = ngaySinh;
+        this.gioiTinh = gioiTinh;
+        this.cmnd = cmnd;
+    }
+
+    public String getHoVaTen() {
+        return hoVaTen;
+    }
+
+    public void setHoVaTen(String hoVaTen) {
+        this.hoVaTen = hoVaTen;
+    }
+
+    public Date getNgaySinh() {
+        return ngaySinh;
+    }
+
+    public void setNgaySinh(Date ngaySinh) {
+        this.ngaySinh = ngaySinh;
+    }
+
+    public String getGioiTinh() {
+        return gioiTinh;
+    }
+
+    public void setGioiTinh(String gioiTinh) {
+        this.gioiTinh = gioiTinh;
+    }
+
+    public int getCmnd() {
+        return cmnd;
+    }
+
+    public void setCmnd(int cmnd) {
+        this.cmnd = cmnd;
+    }
+
+    public void nhapThongTin(Scanner s, SimpleDateFormat fmt) {
+        System.out.print("Nhap ho va ten: ");
+        hoVaTen = s.nextLine();
+        System.out.print("Nhap ngay sinh: ");
+        ngaySinh = new Date();
+        fmt = new SimpleDateFormat("dd/MM/YYY");
+        String strDate = fmt.format(ngaySinh);
+        s.next();
+        System.out.print("Nhap gioi tinh: ");
+        gioiTinh = s.nextLine();
+        s.next();
+        System.out.print("Nhap cmnd: ");
+        cmnd = s.nextInt();
+    }
+
+    public void capNhatNguoiBangCmnd(int cmnd) {
+        System.out.print("Cap nhat cmnd: ");
+        cmnd = s.nextInt();
+    }
+
+    public void xuatThongTin() {
+        System.out.println("Ho va ten: " + hoVaTen);
+        System.out.println("Ngay sinh: " + ngaySinh);
+        System.out.println("Gioi tinh: " + gioiTinh);
+        System.out.println("CMND: " + cmnd);
+    }
+
+    public abstract void traTienDoXe();
+
+    public abstract void suaThongTin(int cmnd, Scanner s);
     public static void main(String[] args) {
-        // TODO code application logic here
+        
     }
     
 }
